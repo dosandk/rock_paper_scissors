e17f977a79a69e99a9e88667064aae119679b942
Volodymyr Shevchuk <dosandkv@gmail.com>
2018-07-09 15:51:22 +0300

    Added dummy tests;

--- app/contracts/Game.sol
+++ app/contracts/Game.sol
@@ -3,13 +3,32 @@ pragma solidity ^0.4.23;
 contract Game {
     uint price = 0.5 ether;
     uint bank = 0;
+    uint timeMark;
 
-    mapping (address => string) figures;
+    struct Player {
+        string figure;
+        uint joiningTime;
+    }
 
-    function join(string _figure) external payable {
-        require(msg.value > price);
-        figures[msg.sender] = _figure;
-        bank += msg.value;
+    mapping (address => Player) addressToPlayer;
+
+    function join(string _figure) external {
+        addressToPlayer[msg.sender] = Player(_figure, now);
+    }
+
+    // Setters
+    // TODO: remove "public" modifier
+    function setPrice(uint _price) public {
+        price = _price;
+    }
+
+    // Getters
+    function getPlayerObject() external view returns(string, uint) {
+        return (addressToPlayer[msg.sender].figure, addressToPlayer[msg.sender].joiningTime);
+    }
+
+    function getFigure() external view returns(string) {
+        return addressToPlayer[msg.sender].figure;
     }
 
     function getAllPlayers() external view returns(uint) {
@@ -17,7 +36,11 @@ contract Game {
         return playersCount;
     }
 
-    function getBankValue() external view returns (uint) {
+    function getBankValue() external view returns(uint) {
         return bank;
+    }
+
+    function getCurrentPrice() external view returns(uint) {
+        return price;
     }
 }
--- app/test/game.spec.js
+++ app/test/game.spec.js
@@ -1,23 +1,53 @@
 const Game = artifacts.require('Game');
 
-contract('Game', accounts => {
-  it('getAllPlayers:: should return 10 value', () => {
-    return Game.deployed()
-      .then(instance => instance.getAllPlayers())
-      .then(data => assert.equal(data, 10, 'was returned another value'));
+contract('Game:', async (accounts) => {
+  let instance;
+
+  beforeEach(async () => {
+    instance = await Game.deployed();
   });
 
-  xit('join:: should add player to game', () => {
-    // TODO: rethink this!
-    // const [accountOne] = accounts;
-    // return Game.deployed()
-    //   .then(instance => instance.join('rock', {from: accountOne, value: 1}))
-    //   .then(() => assert.equal(instance.getBankValue(), 1, ''))
+  it('getAllPlayers:: should return 10 value', async () => {
+      const playersCount = await instance.getAllPlayers();
+
+      assert.equal(playersCount, 10, 'was returned another value');
   });
 
-  it('getBankValue:: should return bank value', () => {
-    return Game.deployed()
-      .then(instance => instance.getBankValue())
-      .then(data => assert.equal(data, 0, 'bunk must be empty!'))
+  it('getBankValue:: should return bank value', async () => {
+      const contractBudget = await instance.getBankValue();
+
+      assert.equal(contractBudget, 0, 'bunk must be empty!');
+  });
+
+  it('getCurrentPrice:: should return price of joining to game', async () => {
+      const price = await instance.getCurrentPrice();
+
+      assert.equal(price.toNumber(), 5e+17, 'default price must be equal 0.5 ether');
+  });
+
+  it('setPrice:: should set price of the game', async () => {
+    const price = 1e+18;
+    await instance.setPrice(price);
+    const currentPrice = await instance.getCurrentPrice();
+
+    assert.equal(currentPrice.toNumber(), price, 'default price must be equal 0.5 ether');
+
+  });
+
+  contract('join:', async () => {
+    let instance;
+
+    beforeEach(async () => {
+      instance = await Game.deployed();
+    });
+
+    it('should add one player to game', async () => {
+      await instance.join('rock');
+
+      const player = await instance.getPlayerObject();
+      const [figure] = player;
+
+      assert.equal(figure, 'rock', 'contract must return value "rock');
+    });
   });
 });
